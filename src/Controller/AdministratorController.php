5cb34b8fc5a714671dfc0897a144cd98f6922700
kyogiamod <richard.torti@usach.cl>
2016-12-14 07:51:05 -0300

    Se agregan las funcionalidad del Manager como buscar task y una mision. Se enecesitan las vistas para poder agregar mas funcionalidades.

--- src/Controller/AdministratorController.php
+++ src/Controller/AdministratorController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Controller;
 
 use Cake\Core\Configure;
@@ -6,16 +7,12 @@ use Cake\Network\Exception\NotFoundException;
 use Cake\View\Exception\MissingTemplateException;
 use Cake\ORM\TableRegistry;
 
+
 class AdministratorController extends AppController
 {
     public function index()
     {
-<<<<<<< HEAD
-    	$this->loadModel('Emergencies');
-    	$this->loadModel('Communes');
-
-=======
-    	
+        
     }
 
     public function view()
@@ -23,26 +20,12 @@ class AdministratorController extends AppController
         $this->loadModel('Emergencies');
         $this->loadModel('Communes');
         
->>>>>>> a29ba30bb542d22f5b3c81a0362363361f4b5ee4
         $emergencies = $this->Emergencies->find('all');
-
         $this->set(compact('comunes'));
         $this->set(compact('emergencies'));
     }
-
+    
     public function logout(){
         return $this->redirect($this->Auth->logout());
     }
-
-    public function addEmergency()
-    {
-    	echo "askdjhaksdhjkads";
-    	exit();
-    }
-
-    public function gestionarPersona($rut)
-    {
-    	exit();
-    }
-
 }
